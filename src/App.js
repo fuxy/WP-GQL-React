5b93897429f2f16dc9ea334003a394c69848797c
Milen Mukanov <milen@fuxy.net>
2019-08-08 22:41:25 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from 'react-apollo';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import Posts from './components/Posts';
-import Post from './components/Post';
+import Posts from "./components/Posts";
+import Post from "./components/Post";
 
 const client = new ApolloClient({
-   uri: 'http://natural-remote-3000.codio.io/backend/graphql'
+  uri: "https://natural-remote-3000.codio.io/backend/graphql"
 });
 
 function App() {
   return (
-      <ApolloProvider client={client}>
-          <BrowserRouter>
-              <div>
-                  <header>
-                      <nav>
-                          <Route exact path="/" component={Posts} />
-                          <Route exact path="/posts" component={Posts} />
-                          <Route exact path="/post/:slug" component={Post} />
-                      </nav>
-                  </header>
-              </div>
-          </BrowserRouter>
-      </ApolloProvider>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <div>
+          <header>
+            <nav>
+              <Route exact path="/" component={Posts} />
+              <Route exact path="/posts" component={Posts} />
+              <Route exact path="/post/:slug" component={Post} />
+            </nav>
+          </header>
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
